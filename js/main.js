31dae15acda4d6fd4f0c90346f0ee4b65d4b3ff0
Michael Mazzone <mazzone.deca@gmail.com>
2017-08-20 12:37:44 -0400

    Most of frontend now complete, ready for backend work

--- js/main.js
+++ js/main.js
@@ -1,3 +1,64 @@
 $(document).ready(function() {
   $('select').material_select();
 });
+
+function getShowName() {
+  var showName = document.getElementById("autocomplete-input").value;
+  console.log("Name: " + showName);
+  return showName;
+}
+
+function initPageData() {
+  /*
+   * Sets page images, links, etc. to the currently selected show, where not
+   * handled in other functions
+  */
+  if(getShowName().length > 1) {
+    // set selected-show-img image
+    // set seasons-span links
+    // set next-episode-span
+    // set next-season-span
+    // set link-wiki and link-imdb
+    // populate amazon-tab, wikibuy-tab, netflix-tab, hulu-tab
+  }
+}
+
+function setShowImg() {
+
+}
+
+function setNumSeasons() {
+
+}
+
+function setNextEpisode() {
+
+}
+
+function setNextSeason() {
+
+}
+
+function setWikiLink() {
+
+}
+
+function setIMDBLink() {
+
+}
+
+function populateAmzn() {
+
+}
+
+function populateWikibuy() {
+
+}
+
+function populateNetflix() {
+
+}
+
+function populateHulu() {
+  
+}
